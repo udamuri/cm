018173507a26d649c8920fe82f9bc36050b5bcfe
muri.budiman <muri.budiman@redawning.com>
2017-01-27 11:12:32 +0700

    get file list with ajax

--- backend/modules/file/models/FileModel.php
+++ backend/modules/file/models/FileModel.php
@@ -10,4 +10,25 @@ use backend\models\TableFile;
 class FileModel extends Model
 {
     
+    public function getFile($search = "")
+    {
+    	 $query = (new \yii\db\Query())
+                    ->select([
+                        'tc.file_id',
+                        'tc.file_name',
+                        'tc.file_folder',
+                        'tc.file_type',
+                        'tc.file_size',
+                        'tc.file_date_upload',
+                        'tc.file_extension',
+                        'tc.user_id'
+                    ])
+                    ->from('tbl_file tc');
+    }
+
+    private function getCountFile()
+    {
+    	$count = TableFile::find()->count();
+    	return $count;
+    }
 }
